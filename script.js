6baaf005ae4dc2b044b1f26085ac823df17a00a1
jesus castro <jesus2401090@hybridge.education>
2025-06-13 00:08:40 -0700

    Segunda version

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const searchInput = document.querySelector("input");
-const buttons = document.querySelectorAll("button");
+const input = document.getElementById('busqueda');
+const btnBuscar = document.getElementById('buscar-btn');
+const btnSuerte = document.getElementById('suerte-btn');
 
-buttons[0].addEventListener("click", () => {
-    window.location.href = `https://www.google.com/search?q=${searchInput.value}`;
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        buscar();
+    }
 });
+
+btnBuscar.addEventListener('click', buscar);
+btnSuerte.addEventListener('click', buscar);
+
+function buscar() {
+    const query = encodeURIComponent(input.value.trim());
+    if (query) {
+        window.location.href = `https://google.com/search?q=${query}`;
+    }
+}
